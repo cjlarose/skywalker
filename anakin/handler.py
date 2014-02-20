69173bc51ffb97c0e59d90379a321eb4ff3d50ff
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-02-20 14:29:49 -0700

    Add list_all_instances method to handler

--- anakin/handler.py
+++ anakin/handler.py
@@ -1,4 +1,11 @@
+from atmo_soa.anakin.ttypes import Instance
+
 class Handler(object):
 
     def __init__(self):
         pass
+
+    def list_all_instances(self, driver):
+        return [
+            Instance(id='1234')
+        ]
