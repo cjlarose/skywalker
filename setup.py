91040c56981c04fbd55d240f45acc33d0203de31
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-02-20 10:05:28 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='anakin',
+    version='0.1',
+    description='Cloud Interface as a Service',
+    author='iPlantCollaborative',
+    author_email='cjlarose@email.arizona.edu',
+    url='http://iplantcollaborative.org',
+    packages=find_packages()
+    )
